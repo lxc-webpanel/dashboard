54b241e40e8a3f12459f5dc61cbe73e7da547ce5
Antoine <antoine@tanzil.li>
2017-01-24 00:15:41 +0100

    Add 'https' option to Webpack dev server

--- server/main.js
+++ server/main.js
@@ -25,7 +25,8 @@ if (project.env === 'development') {
     quiet       : project.compiler_quiet,
     noInfo      : project.compiler_quiet,
     lazy        : false,
-    stats       : project.compiler_stats
+    stats       : project.compiler_stats,
+    https       : process.env.HTTPS || false
   }));
   app.use(require('webpack-hot-middleware')(compiler, {
     path: '/__webpack_hmr'
